dbdc38589a1a5daf273fc710cd0101dd41454b31
Cheng-Yu Hsu <m@cyhsu.me>
2016-09-24 09:19:44 +0800

    Add POST spec

--- spec/helpers/http-request-helper.spec.js
+++ spec/helpers/http-request-helper.spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { get } from '../../src/libs/helpers/http-request-helper'
+import { get, post } from '../../src/libs/helpers/http-request-helper'
 
 const REQUEST_TIMEOUT = 15 * 1000 // 15 seconds
 
@@ -18,7 +18,41 @@ describe('get()', function () { // must NOT use arrow function
     it('returns a 404', () => {
       return get('https://raw.githubusercontent.com/muan/emojilib/master/emojis.json.404')
         .then((response) => {
-          throw new Error('Success status captured')
+          throw new Error('unexpected success status captured')
+        })
+        .catch((statusCode) => {
+          expect(statusCode).to.equal(404)
+        })
+    })
+  })
+})
+
+describe('post()', function () {
+  this.timeout(REQUEST_TIMEOUT)
+
+  const payload = {
+    name: 'BrakeValve',
+    type: 'software',
+    links: [
+      'https://github.com/brakevalve',
+      'https://brakevalve.github.io/'
+    ]
+  }
+
+  describe('with good url', () => {
+    it('returns success', () => {
+      return post('https://httpbin.org/post', payload)
+        .then((response) => {
+          expect(response.json).to.deep.equal(payload)
+        })
+    })
+  })
+
+  describe('with bad url', function () {
+    it('returns a 404', function () {
+      return post('https://httpbin.org/post.404', payload)
+        .then((response) => {
+          throw new Error('unexpected success status captured')
         })
         .catch((statusCode) => {
           expect(statusCode).to.equal(404)
