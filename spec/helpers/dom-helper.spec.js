009ffbd29a018e0abdc4884fd7dc739167ea3a30
Cheng-Yu Hsu <m@cyhsu.me>
2016-09-26 14:59:40 +0800

    Add test for getAppID

--- spec/helpers/dom-helper.spec.js
+++ spec/helpers/dom-helper.spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { getCurrency } from '../../src/libs/helpers/dom-helper'
+import { getCurrency, getAppID } from '../../src/libs/helpers/dom-helper'
 
 describe('getCurrency()', () => {
   afterEach(() => {
@@ -37,6 +37,41 @@ describe('getCurrency()', () => {
   describe('without meta tag', () => {
     it('returns null', () => {
       expect(getCurrency()).to.be.null
+    })
+  })
+})
+
+describe('getAppID()', () => {
+  describe('with valid input tag', () => {
+    const appID = 10
+
+    before(() => {
+      const inputTag = document.createElement('input')
+      inputTag.setAttribute('type', 'hidden')
+      inputTag.setAttribute('id', 'review_appid')
+      inputTag.setAttribute('value', appID)
+    })
+
+    it('returns app the desired app id', () => {
+      expect(getAppID()).to.equal(appID)
+    })
+  })
+
+  describe('with invalid input tag (missing attribute: value)', () => {
+    before(() => {
+      const inputTag = document.createElement('input')
+      inputTag.setAttribute('type', 'hidden')
+      inputTag.setAttribute('id', 'review_appid')
+    })
+
+    it('returns null', () => {
+      expect(getAppID()).to.be.null
+    })
+  })
+
+  describe('without input tag', () => {
+    it('returns null', () => {
+      expect(getAppID()).to.be.null
     })
   })
 })
